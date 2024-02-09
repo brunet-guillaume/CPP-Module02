4c23747ccae60d7255c137bb314c24c942e6294a
Guillaume Brunet <gbrunet@student.42.fr>
2024-02-09 17:21:24 +0100

    a starting point

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbrunet <gbrunet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 16:31:49 by gbrunet           #+#    #+#             */
+/*   Updated: 2024/02/09 16:34:47 by gbrunet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed {
+	public:
+		Fixed();
+		Fixed(const Fixed &fixed);
+		~Fixed();
+		Fixed &operator=(const Fixed &fixed);
+};
+
+#endif
