77f953b2c72d1ebb65ad948e4877ebaaf3bbf5ce
Guillaume Brunet <gbrunet@student.42.fr>
2024-02-12 10:46:01 +0100

    ex02 - WIP

---
+++ ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbrunet <gbrunet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 16:31:41 by gbrunet           #+#    #+#             */
+/*   Updated: 2024/02/12 10:45:07 by gbrunet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int	main(void) {
+	Fixed		a;
+	Fixed const	b(10);
+	Fixed const	c(42.42f);
+	Fixed const	d(b);
+
+	a = Fixed(1234.4321f);
+
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << a + b << std::endl;
+	std::cout << a - b << std::endl;
+	std::cout << a * b << std::endl;
+	std::cout << a / b << std::endl;
+	
+	return(0);
+}
